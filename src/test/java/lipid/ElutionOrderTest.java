517ffa43444a471e66cb188e75b36efc173d52c1
alejandraoshea <alejandra.osheafernandez@usp.ceu.es>
2025-05-08 10:25:56 +0200

    test

--- src/test/java/lipid/ElutionOrderTest.java
+++ src/test/java/lipid/ElutionOrderTest.java
@@ -27,46 +27,6 @@ public class ElutionOrderTest {
     }
 
 
-    @Test
-    public void score1BasedOnRT() {
-        // Assume lipids already annotated
-        LOG.info("Creating RuleUnit");
-        LipidScoreUnit lipidScoreUnit = new LipidScoreUnit();
-
-        RuleUnitInstance<LipidScoreUnit> instance = RuleUnitProvider.get().createRuleUnitInstance(lipidScoreUnit);
-
-
-        // TODO CHECK THE Monoisotopic MASSES OF THE COMPOUNDS IN https://chemcalc.org/
-        Lipid lipid1 = new Lipid(1, "TG 54:3", "C57H104O6", "TG", 54, 3); // MZ of [M+H]+ = 885.79057
-        Lipid lipid2 = new Lipid(2, "TG 52:3", "C55H100O6", "TG", 52, 3); // MZ of [M+H]+ = 857.75927
-        Lipid lipid3 = new Lipid(3, "TG 56:3", "C59H108O6", "TG", 56, 3); // MZ of [M+H]+ = 913.82187
-        Annotation annotation1 = new Annotation(lipid1, 885.79056, 10E6, 10d, IonizationMode.POSITIVE);
-        Annotation annotation2 = new Annotation(lipid2, 857.7593, 10E7, 9d, IonizationMode.POSITIVE);
-        Annotation annotation3 = new Annotation(lipid3, 913.822, 10E5, 11d, IonizationMode.POSITIVE);
-
-        LOG.info("Insert data");
-
-        try {
-            lipidScoreUnit.getAnnotations().add(annotation1);
-            lipidScoreUnit.getAnnotations().add(annotation2);
-            lipidScoreUnit.getAnnotations().add(annotation3);
-
-            LOG.info("Run query. Rules are also fired");
-            instance.fire();
-
-            // Here the logic that we expect. In this case we expect the full 3 annotations to have a positive score of 1
-
-            assertEquals(1.0, annotation1.getNormalizedScore(), 0.01);
-            assertEquals(1.0, annotation2.getNormalizedScore(), 0.01);
-            assertEquals(1.0, annotation3.getNormalizedScore(), 0.01);
-
-        }
-        finally {
-            instance.close();
-        }
-    }
-
-
     /**
      * Test to check the elution order of the lipids. The elution order is based on the number of carbons if the lipid type and the number of
      * double bonds is the same. The larger the number of carbons, the longer the RT.
